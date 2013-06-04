05dcde85bb1d0c26f23f7cad231d12556541dd0d
Pascal Precht <pascal.precht@googlemail.com>
2013-06-04 14:16:00 +0200

    feat(esthetic): add optional options
    the directive is now able to understand given options via
    
    <ANY esthetic={options}></ANY>

--- src/esthetic.js
+++ src/esthetic.js
@@ -1,27 +1,56 @@
-angular.module('pascalprecht.esthetic', ['ng']);
+angular.module('pascalprecht.esthetic-adapter', ['ng']);
 /**
  * @ngdoc directive
- * @name pascalprecht.esthetic.directive:esthetic
+ * @name pascalprecht.esthetic-adapter.directive:esthetic
+ * @requires $timeout
  * @restrict A
  *
  * @description
- * Brings esthetic functionality to angular world.
+ * Brings esthetic functionality to angular world. The directive is restricted as
+ * attribute. It is also possible to pass options by using the esthetic-options
+ * attribute.
  *
+ * @param {string=} estethic Options to pass into esthetic plugin as string as object literal.
+ *
+ * @example
+   <example module="ngView">
+    <file name="index.html">
+      <div ng-controller="EstheticCtrl">
+        <select
+          ng-model="selected"
+          ng-options="value.id as value.label for (key, value) in options"
+          esthetic="{btnClass: 'foo', itemClass: 'bar'}"
+          >
+        </select>
+      </div>
+    </file>
+    <file name="script.js">
+      angular.module('ngView', ['pascalprecht.esthetic-adapter'])
+
+      .controller('EstheticCtrl', function ($scope) {
+        $scope.selected = 2;
+        $scope.options = [
+          { id: 1, label: 'foo' },
+          { id: 2, label: 'bar' }
+        ];
+      });
+    </file>
+   </example>
  */
-angular.module('pascalprecht.esthetic').directive('esthetic', ['$timeout', function ($timeout) {
+angular.module('pascalprecht.esthetic-adapter').directive('esthetic', ['$timeout', function ($timeout) {
   return {
     restrict: 'A',
-    scope: true,
     link: function (scope, element, attrs) {
 
-      var esthetic;
+      var esthetic,
+          options = scope.$eval(attrs['esthetic']);
 
       $timeout(function () {
-        element.esthetic();
+        element.esthetic(options);
         esthetic = element.data('esthetic');
-      }
+      });
 
-      attrs.$observe('ngModel', function (value) {
+      scope.$watch(attrs['ngModel'], function (value) {
         if (value) {
           $timeout(function () {
             esthetic.update();

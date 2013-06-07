8ad68ee715153ee1c982521e3c7f90a16e90bf1c
Pascal Precht <pascal.precht@googlemail.com>
2013-06-07 11:11:31 +0200

    fix(): fixes bug that ng-options change dont trigger anything

--- src/esthetic.js
+++ src/esthetic.js
@@ -3,6 +3,7 @@ angular.module('pascalprecht.esthetic-adapter', ['ng']);
  * @ngdoc directive
  * @name pascalprecht.esthetic-adapter.directive:esthetic
  * @requires $timeout
+ * @requires $parse
  * @restrict A
  *
  * @description
@@ -37,20 +38,29 @@ angular.module('pascalprecht.esthetic-adapter', ['ng']);
     </file>
    </example>
  */
-angular.module('pascalprecht.esthetic-adapter').directive('esthetic', ['$timeout', function ($timeout) {
+angular.module('pascalprecht.esthetic-adapter').directive('esthetic', ['$timeout', '$parse', function ($timeout, $parse) {
   return {
     restrict: 'A',
     link: function (scope, element, attrs) {
-
-      var esthetic,
-          options = scope.$eval(attrs['esthetic']);
+      var NG_OPTIONS_REGEXP = /^\s*(.*?)(?:\s+as\s+(.*?))?(?:\s+group\s+by\s+(.*))?\s+for\s+(?:([\$\w][\$\w\d]*)|(?:\(\s*([\$\w][\$\w\d]*)\s*,\s*([\$\w][\$\w\d]*)\s*\)))\s+in\s+(.*?)(?:\s+track\s+by\s+(.*?))?$/,
+          esthetic,
+          options = scope.$eval(attrs.esthetic),
+          match = attrs.ngOptions.match(NG_OPTIONS_REGEXP);
 
       $timeout(function () {
         element.esthetic(options);
         esthetic = element.data('esthetic');
       });
 
-      scope.$watch(attrs['ngModel'], function (value) {
+      scope.$watch(attrs.ngModel, function (value) {
+        if (value) {
+          $timeout(function () {
+            esthetic.update();
+          });
+        }
+      });
+
+      scope.$watch($parse(match[7]), function (value) {
         if (value) {
           $timeout(function () {
             esthetic.update();
